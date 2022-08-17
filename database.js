4a6d4b113709e8ae3330ae037fd5347c828747a6
hjy <heojungyeon.97@gmail.com>
2022-08-17 22:35:23 +0900

    Feat: 서버 모듈화

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const csvToJson = require("convert-csv-to-json");
+
 const database = {
   teams: [],
   people: [],
@@ -7,9 +8,9 @@ const database = {
   equipments: [],
   supplies: [],
 };
-
 Object.keys(database).forEach((key) => {
   database[key] = [...database[key], ...csvToJson.fieldDelimiter(",").getJsonFromCsv(`./data-in-csv/${key}.csv`)];
+  // 숫자인 항목은 타입을 Number로 변형
   if (database[key].length > 0) {
     const firstItem = database[key][0];
     Object.keys(firstItem).forEach((itemKey) => {
